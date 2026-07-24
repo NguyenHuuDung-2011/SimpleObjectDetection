b03df7d033c5f4ee3581635b257942d39200753c
NguyenHuuDung-2011 <huudung5c@gmail.com>
2026-07-24 20:58:11 +0700

    Adding AI analysis and detected objects in response

--- camera/views.py
+++ camera/views.py
@@ -1,6 +1,11 @@
 from django.http import JsonResponse
 from django.middleware.csrf import get_token
 from django.shortcuts import render
+import cv2
+import numpy as np
+from ultralytics import YOLO
+
+model = YOLO('yolo11n.pt')
 
 # Create your views here.
 def home(request):
@@ -8,8 +13,30 @@ def home(request):
     return render(request, 'home.html')
 
 def capture(request):
+    detections = []
     if request.method == 'POST':
         if 'image' in request.FILES:
             image = request.FILES['image']
+            image = image.read()
+            image = np.frombuffer(image, dtype=np.uint8)
+            frame = cv2.imdecode(image, cv2.IMREAD_COLOR)
+            results = model(frame)
 
-    return JsonResponse({'status': 'ok'})
+            for result in results:
+                for box in result.boxes:
+                    x1, y1, x2, y2 = map(int, box.xyxy[0])
+                    cls_id = int(box.cls[0])
+                    label = model.names[cls_id]
+                    detections.append({
+                        'class': label,
+                        'conf': float(box.conf[0]),
+                        'x1': x1,
+                        'y1': y1,
+                        'x2': x2,
+                        'y2': y2
+                    })
+
+    return JsonResponse({
+        'detections': detections,
+        'status': 'ok'
+    })
